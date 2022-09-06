17edb4bd4a8611fc007fb01f844dc88e5ffe4014
ilya.kuznetosv <kromsik@icloud.com>
2022-09-06 19:24:48 +0300

    📦 NEW: strange AppBar

--- lib/ui/screens/sight_list_screen.dart
+++ lib/ui/screens/sight_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:places/ui/styleguide/styleguide.dart';
 
 class SightListScreen extends StatefulWidget {
   const SightListScreen({Key? key}) : super(key: key);
@@ -11,11 +12,63 @@ class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text('Flutter Surf course'),
+        centerTitle: false,
+        backgroundColor: Colors.white,
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(72.0),
+          child: Container(
+            child: const FancyText(),
+            alignment: Alignment.centerLeft,
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+          ),
+        ),
+        elevation: 0,
       ),
-      body: const Center(
-        child: Text('Hello'),
+      body: Container(
+        child: Container(),
+      ),
+    );
+  }
+}
+
+class FancyText extends StatelessWidget {
+  const FancyText({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: const TextSpan(
+        text: 'С',
+        style: TextStyle(
+          color: CustomColorPalette.green,
+          fontFamily: "Roboto",
+          fontSize: 32,
+          fontWeight: FontWeight.w700,
+          height: 112 / 100,
+        ),
+        children: [
+          TextSpan(
+            text: "писок",
+            style: TextStyle(
+              color: CustomColorPalette.textPrimary,
+            ),
+          ),
+          TextSpan(text: "\n"),
+          TextSpan(
+            text: "и",
+            style: TextStyle(
+              color: CustomColorPalette.yellow,
+            ),
+          ),
+          TextSpan(
+            text: "нтересных мест",
+            style: TextStyle(
+              color: CustomColorPalette.textPrimary,
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/ui/styleguide/styleguide.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class CustomColorPalette {
+  static const Color textPrimary = Color.fromRGBO(37, 40, 73, 1);
+  static const Color green = Color.fromRGBO(76, 175, 80, 1);
+  static const Color yellow = Color.fromRGBO(252, 221, 61, 1);
+}
