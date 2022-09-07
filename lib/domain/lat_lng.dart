8ce655ec77e8d8c4eea9089bd3d1c1b5773a9972
ilya.kuznetosv <kromsik@icloud.com>
2022-09-07 19:18:27 +0300

    📖 DOC: models

---
+++ lib/domain/lat_lng.dart
@@ -0,0 +1,6 @@
+class LatLng {
+  final double lat;
+  final double lgn;
+
+  const LatLng({required this.lat, required this.lgn});
+}
---
+++ lib/domain/sight.dart
@@ -0,0 +1,36 @@
+import 'package:places/domain/lat_lng.dart';
+
+class Sight {
+  final String name; // -  название достопримечательности
+  final LatLng location;
+  final String url; //- путь до фотографии в интернете
+  final String details; //- описание достопримечательности
+  final SightType type; // тип достопримечательности.
+
+  const Sight({
+    required this.name,
+    required this.location,
+    required this.url,
+    required this.details,
+    required this.type,
+  });
+}
+
+enum SightType {
+  scyscrapper,
+  square,
+  monument,
+}
+
+extension SightTypeExt on SightType {
+  String get name {
+    switch (this) {
+      case SightType.scyscrapper:
+        return 'scyscrapper';
+      case SightType.square:
+        return 'square';
+      case SightType.monument:
+        return 'monument';
+    }
+  }
+}
